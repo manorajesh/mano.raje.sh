a97759345694d7db0d8f28896a6273031a387df9
manorajesh <mano@raje.sh>
2023-09-09 22:28:58 -0700

    Basic layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import cloudflare from "@astrojs/cloudflare";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [mdx()]
 });
