43977635308cd2248f083afe0847082b05a4c053
manorajesh <mano@raje.sh>
2023-12-28 11:34:26 -0700

    Reverse proxy works

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,13 @@
 use actix_web::{ get, post, web, App, HttpResponse, HttpServer, Responder };
+use actix_cors::Cors;
 
-#[get("/")]
+#[get("/api/hello")]
 async fn hello() -> impl Responder {
+    println!("hello");
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/echo")]
+#[post("/api/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
@@ -17,7 +19,12 @@ async fn manual_hello() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(hello).service(echo).route("/hey", web::get().to(manual_hello))
+        let cors = Cors::permissive();
+        App::new()
+            .wrap(cors)
+            .service(hello)
+            .service(echo)
+            .route("/api/hey", web::get().to(manual_hello))
     })
         .bind(("0.0.0.0", 8080))?
         .run().await
